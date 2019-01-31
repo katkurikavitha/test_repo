389fe04bd1c5195852c055087f18e4217c64a869
katkurikavitha <kavitha.katkuri@appitventures.com>
2019-01-31 14:12:20 +0530

    added  Xcode

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Malathi on 1/31/19.
+//  Copyright © 2019 Appitventures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
