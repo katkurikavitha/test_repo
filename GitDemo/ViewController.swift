6600d300bdc88b4bdd25d33b1890f20874c853f9
katkurikavitha <kavitha.katkuri@appitventures.com>
2019-01-31 15:53:58 +0530

    added buttom

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var btn = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
