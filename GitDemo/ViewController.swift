10894af5134b0e21172e328c9ab79cd504afb252
katkurikavitha <kavitha.katkuri@appitventures.com>
2019-01-31 17:04:23 +0530

    added btn by Rashida

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     var btn = UIButton()
     // added by Kavitha
     // added by Rashida
+    
+var rashidaButton = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
